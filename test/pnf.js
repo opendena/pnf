0661f53f2cb475e2ac764899a23b2486d919c212
Sébastien ELET <sebastien@elet.fr>
2014-02-19 22:01:51 +0100

    Convert tests into javascript

---
+++ test/pnf.js
@@ -0,0 +1,164 @@
+(function() {
+  "use strict";
+  var assert = require('assert')
+    , exec = require('child_process').exec
+    , os = require('os')
+    , pnf = './bin/pnf';
+
+  describe("Formats phone number", function() {
+    describe("Display the help", function() {
+      it("Should diplay help text", function(done) {
+        exec("" + pnf + " --help", function(error, stdout, stderr) {
+          assert.equal(stderr, "");
+          assert.equal(
+            stdout,
+            [
+              'Usage: pnf [options] [numbers...]'
+              , '   or: [numbers...] | pnf [options]'
+              , ''
+              , 'Description:'
+              , ''
+              , 'Phone number format'
+              , ''
+              , 'Options'
+              , ''
+              , '  -intl, --intl            Internationnal format'
+              , '  -e164, --e164            e164 format (default)'
+              , '  -lang, --lang            Language (ISO 639-1, default is FR)'
+              , ''
+            ].join(os.EOL)
+          );
+          done();
+        });
+      });
+    });
+    describe("Error from stdin", function() {
+      it("Should return an error for 33", function(done) {
+        exec("echo 33 | " + pnf, function(error, stdout, stderr) {
+          assert.equal(stderr, "33 is not a valid number" + os.EOL);
+          assert.equal(stdout, "");
+          done();
+        });
+      });
+    });
+    describe("Error from args", function() {
+      it("Should return an error for 33", function(done) {
+        exec("" + pnf + " 33", function(error, stdout, stderr) {
+          assert.equal(stderr, "33 is not a valid number" + os.EOL);
+          assert.equal(stdout, "");
+          done();
+        });
+      });
+    });
+    describe("Default (e164) from stdin", function() {
+      it("Should return +33364515012", function(done) {
+        exec("echo 33364515012 | " + pnf, function(error, stdout, stderr) {
+          assert.equal(stderr, "");
+          assert.equal(stdout, "+33364515012" + os.EOL);
+          done();
+        });
+      });
+      it("Should return +33364515012", function(done) {
+        exec("echo 0364515012 | " + pnf + " ", function(error, stdout, stderr) {
+          assert.equal(stderr, "");
+          assert.equal(stdout, "+33364515012" + os.EOL);
+          done();
+        });
+      });
+      it("Should return +33364515012", function(done) {
+        exec("echo 364515012 | " + pnf + " ", function(error, stdout, stderr) {
+          assert.equal(stderr, "");
+          assert.equal(stdout, "+33364515012" + os.EOL);
+          done();
+        });
+      });
+    });
+    describe("Default (e164) from args", function() {
+      it("Should return +33364515012", function(done) {
+        exec("" + pnf + " 33364515012", function(error, stdout, stderr) {
+          assert.equal(stderr, "");
+          assert.equal(stdout, "+33364515012" + os.EOL);
+          done();
+        });
+      });
+      it("Should return +33364515012", function(done) {
+        exec("" + pnf + " 0364515012", function(error, stdout, stderr) {
+          assert.equal(stderr, "");
+          assert.equal(stdout, "+33364515012" + os.EOL);
+          done();
+        });
+      });
+      it("Should return +33364515012", function(done) {
+        exec("" + pnf + " 364515012", function(error, stdout, stderr) {
+          assert.equal(stderr, "");
+          assert.equal(stdout, "+33364515012" + os.EOL);
+          done();
+        });
+      });
+    });
+    describe("International from stdin", function() {
+      it("Should return +33 3 64 51 50 12", function(done) {
+        exec(
+          "echo 33364515012 | " + pnf + " -international",
+          function(error, stdout, stderr) {
+            assert.equal(stderr, "");
+            assert.equal(stdout, "+33 3 64 51 50 12" + os.EOL);
+            done();
+          }
+        );
+      });
+      it("Should return +33 3 64 51 50 12", function(done) {
+        exec(
+          "echo 0364515012 | " + pnf + " -international",
+          function(error, stdout, stderr) {
+            assert.equal(stderr, "");
+            assert.equal(stdout, "+33 3 64 51 50 12" + os.EOL);
+            done();
+          }
+        );
+      });
+      it("Should return +33 3 64 51 50 12", function(done) {
+        exec(
+          "echo 364515012 | " + pnf + " -international",
+          function(error, stdout, stderr) {
+            assert.equal(stderr, "");
+            assert.equal(stdout, "+33 3 64 51 50 12" + os.EOL);
+            done();
+          }
+        );
+      });
+    });
+    describe("International from args", function() {
+      it("Should return +33 3 64 51 50 12", function(done) {
+        exec(
+          "" + pnf + " -international 33364515012",
+          function(error, stdout, stderr) {
+            assert.equal(stderr, "");
+            assert.equal(stdout, "+33 3 64 51 50 12" + os.EOL);
+            done();
+          }
+        );
+      });
+      it("Should return +33 3 64 51 50 12", function(done) {
+        exec(
+          "" + pnf + " -international 0364515012",
+          function(error, stdout, stderr) {
+            assert.equal(stderr, "");
+            assert.equal(stdout, "+33 3 64 51 50 12" + os.EOL);
+            done();
+          });
+      });
+      it("Should return +33 3 64 51 50 12", function(done) {
+        exec(
+          "" + pnf + " -international 364515012",
+          function(error, stdout, stderr) {
+            assert.equal(stderr, "");
+            assert.equal(stdout, "+33 3 64 51 50 12" + os.EOL);
+            done();
+          }
+        );
+      });
+    });
+  });
+
+}).call(this);
