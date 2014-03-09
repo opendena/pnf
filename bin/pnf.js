0eb20f96ee08f0ab9227f174cbfc71db58c10079
Sébastien ELET <sebastien@elet.fr>
2014-03-09 20:29:30 +0100

    Refactor the project to add code coverage

--- bin/pnf.js
+++ bin/pnf.js
@@ -1,92 +1,14 @@
 #!/usr/bin/env node
 "use strict";
-var libphonenumber = require('libphonenumber');
-var readline = require('readline');
-var os = require('os');
-var pnf = {};
-var numbers = [];
 
-pnf.options = {
-  format: 'e164',
-  lang: 'FR'
-}
-pnf.format = function(number) {
-  libphonenumber[this.options.format](
-    number,
-    this.options.lang,
-    function(error, result) {
-      if (error) {
-        console.error(
-          number + " is not a valid number"
-        );
-      }
-      if (result) {
-        console.log(result);
-      }
-    }
-  )
-}
+var pnf = require('../lib/pnf.js')
 
-process.argv.shift()
-process.argv.shift()
-process.argv.forEach(function(arg) {
-  switch(true) {
-    case arg == '-help':
-    case arg == '--help':
-      console.log(
-        [
-          'Usage: pnf [options] [numbers...]',
-          '   or: [numbers...] | pnf [options]',
-          '',
-          'Description:',
-          '',
-          'Phone number format',
-          '',
-          'Options',
-          '',
-          '  -intl, --intl            Internationnal format',
-          '  -e164, --e164            e164 format (default)',
-          '  -lang, --lang            Language (ISO 639-1, default is FR)'
-        ].join(os.EOL)
-      );
-      process.exit(0);
-      break;
-    case arg == '-intl':
-    case arg == '--intl':
-    case arg == '-international':
-    case arg == '--international':
-      pnf.options.format = 'intl';
-      break;
-    case arg == '-e164':
-    case arg == '--e164':
-      pnf.options.format = 'e164';
-      break;
-    case arg.substr(0,6) == '-lang=':
-    case arg.substr(0,7) == '--lang=':
-      pnf.options.lang = arg.split('=')[1];
-      break;
-    default:
-      numbers.push(arg);
-      break;
-  }
+pnf.config({
+  argv: process.argv,
+  stdin: process.stdin,
+  stdout: process.stdout,
+  stderr: process.stderr
 })
-
-if (numbers.length === 0) {
-  var rl = readline.createInterface({
-    input: process.stdin,
-    output: function() {}
-  });
-
-  rl.on('line', function(line) {
-    line = line.replace(/\r?\n|\r/g, ''); // Nodejs 0.8 issue
-    pnf.format(line)
-  })
-
-  rl.on('end', function() {
-    process.exit(0);
-  });
-} else {
-  numbers.forEach(function(number) {
-    pnf.format(number)
-  })
-}
+pnf.run(function() {
+  process.exit(0)
+})
--- test/pnf.js
+++ test/pnf.js
@@ -3,12 +3,14 @@
   var assert = require('assert')
     , exec = require('child_process').exec
     , os = require('os')
-    , pnf = './bin/pnf.js';
+    , stream = require('mock-utf8-stream')
+    , bin = './bin/pnf.js'
+    , pnf = require('../lib/pnf.js');
 
   describe("Formats phone number", function() {
     describe("Display the help", function() {
       it("Should diplay help text", function(done) {
-        exec("" + pnf + " --help", function(error, stdout, stderr) {
+        exec("" + bin + " --help", function(error, stdout, stderr) {
           assert.equal(stderr, "");
           assert.equal(
             stdout,
@@ -34,7 +36,7 @@
     });
     describe("Error from stdin", function() {
       it("Should return an error for 33", function(done) {
-        exec("echo 33 | " + pnf, function(error, stdout, stderr) {
+        exec("echo 33 | " + bin, function(error, stdout, stderr) {
           assert.equal(stderr, "33 is not a valid number" + os.EOL);
           assert.equal(stdout, "");
           done();
@@ -43,7 +45,7 @@
     });
     describe("Error from args", function() {
       it("Should return an error for 33", function(done) {
-        exec("" + pnf + " 33", function(error, stdout, stderr) {
+        exec("" + bin + " 33", function(error, stdout, stderr) {
           assert.equal(stderr, "33 is not a valid number" + os.EOL);
           assert.equal(stdout, "");
           done();
@@ -52,21 +54,39 @@
     });
     describe("Default (e164) from stdin", function() {
       it("Should return +33364515012", function(done) {
-        exec("echo 33364515012 | " + pnf, function(error, stdout, stderr) {
+        exec("echo 33364515012 | " + bin, function(error, stdout, stderr) {
           assert.equal(stderr, "");
           assert.equal(stdout, "+33364515012" + os.EOL);
           done();
         });
       });
       it("Should return +33364515012", function(done) {
-        exec("echo 0364515012 | " + pnf + " ", function(error, stdout, stderr) {
+        var stdout = new stream.MockWritableStream(),
+          stderr = new stream.MockWritableStream(),
+          stdin = new stream.MockReadableStream()
+        stdout.startCapture()
+        stderr.startCapture()
+        pnf.config({
+          stdin: stdin,
+          stdout: stdout,
+          stderr: stderr,
+          argv: [
+            'node',
+            'pnf.js'
+          ]
+        })
+        done()
+      });
+
+      it("Should return +33364515012", function(done) {
+        exec("echo 0364515012 | " + bin + " ", function(error, stdout, stderr) {
           assert.equal(stderr, "");
           assert.equal(stdout, "+33364515012" + os.EOL);
           done();
         });
       });
       it("Should return +33364515012", function(done) {
-        exec("echo 364515012 | " + pnf + " ", function(error, stdout, stderr) {
+        exec("echo 364515012 | " + bin + " ", function(error, stdout, stderr) {
           assert.equal(stderr, "");
           assert.equal(stdout, "+33364515012" + os.EOL);
           done();
@@ -75,21 +95,45 @@
     });
     describe("Default (e164) from args", function() {
       it("Should return +33364515012", function(done) {
-        exec("" + pnf + " 33364515012", function(error, stdout, stderr) {
+        exec("" + bin + " 33364515012", function(error, stdout, stderr) {
           assert.equal(stderr, "");
           assert.equal(stdout, "+33364515012" + os.EOL);
           done();
         });
       });
       it("Should return +33364515012", function(done) {
-        exec("" + pnf + " 0364515012", function(error, stdout, stderr) {
+        var stdout = new stream.MockWritableStream(),
+          stderr = new stream.MockWritableStream(),
+          stdin = new stream.MockReadableStream()
+        stdout.startCapture()
+        stderr.startCapture()
+        pnf.config({
+          stdin: stdin,
+          stdout: stdout,
+          stderr: stderr,
+          argv: [
+            'node',
+            'pnf.js',
+            '33364515012'
+          ]
+        })
+        pnf.run(function() {
+          assert.equal(stderr.capturedData, '')
+          assert.equal(stdout.capturedData, '+33364515012' + os.EOL)
+          done()
+        })
+      });
+
+      it("Should return +33364515012", function(done) {
+        exec("" + bin + " 0364515012", function(error, stdout, stderr) {
           assert.equal(stderr, "");
           assert.equal(stdout, "+33364515012" + os.EOL);
           done();
         });
       });
+
       it("Should return +33364515012", function(done) {
-        exec("" + pnf + " 364515012", function(error, stdout, stderr) {
+        exec("" + bin + " 364515012", function(error, stdout, stderr) {
           assert.equal(stderr, "");
           assert.equal(stdout, "+33364515012" + os.EOL);
           done();
@@ -99,7 +143,7 @@
     describe("International from stdin", function() {
       it("Should return +33 3 64 51 50 12", function(done) {
         exec(
-          "echo 33364515012 | " + pnf + " -international",
+          "echo 33364515012 | " + bin + " -international",
           function(error, stdout, stderr) {
             assert.equal(stderr, "");
             assert.equal(stdout, "+33 3 64 51 50 12" + os.EOL);
@@ -109,7 +153,7 @@
       });
       it("Should return +33 3 64 51 50 12", function(done) {
         exec(
-          "echo 0364515012 | " + pnf + " -international",
+          "echo 0364515012 | " + bin + " -international",
           function(error, stdout, stderr) {
             assert.equal(stderr, "");
             assert.equal(stdout, "+33 3 64 51 50 12" + os.EOL);
@@ -119,7 +163,7 @@
       });
       it("Should return +33 3 64 51 50 12", function(done) {
         exec(
-          "echo 364515012 | " + pnf + " -international",
+          "echo 364515012 | " + bin + " -international",
           function(error, stdout, stderr) {
             assert.equal(stderr, "");
             assert.equal(stdout, "+33 3 64 51 50 12" + os.EOL);
@@ -131,7 +175,7 @@
     describe("International from args", function() {
       it("Should return +33 3 64 51 50 12", function(done) {
         exec(
-          "" + pnf + " -international 33364515012",
+          "" + bin + " -international 33364515012",
           function(error, stdout, stderr) {
             assert.equal(stderr, "");
             assert.equal(stdout, "+33 3 64 51 50 12" + os.EOL);
@@ -141,7 +185,7 @@
       });
       it("Should return +33 3 64 51 50 12", function(done) {
         exec(
-          "" + pnf + " -international 0364515012",
+          "" + bin + " -international 0364515012",
           function(error, stdout, stderr) {
             assert.equal(stderr, "");
             assert.equal(stdout, "+33 3 64 51 50 12" + os.EOL);
@@ -150,7 +194,7 @@
       });
       it("Should return +33 3 64 51 50 12", function(done) {
         exec(
-          "" + pnf + " -international 364515012",
+          "" + bin + " -international 364515012",
           function(error, stdout, stderr) {
             assert.equal(stderr, "");
             assert.equal(stdout, "+33 3 64 51 50 12" + os.EOL);
