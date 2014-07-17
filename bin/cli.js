56e8400d4405fefb84af2a7e545f8e01c7624b50
Sébastien ELET <sebastien@elet.fr>
2014-07-17 18:43:21 +0200

    Split the code for the bin for a better javascript parsing (shebang could hit some parsers)

---
+++ bin/cli.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+'use strict';
+
+var pnf = require('../lib/pnf.js')
+
+pnf.config({
+  argv: process.argv,
+  stdin: process.stdin,
+  stdout: process.stdout,
+  stderr: process.stderr
+})
+pnf.run(function() {
+  process.exit(0)
+})
--- bin/pnf.js
+++ bin/pnf.js
@@ -1,14 +1,3 @@
 #!/usr/bin/env node
 'use strict';
-
-var pnf = require('../lib/pnf.js')
-
-pnf.config({
-  argv: process.argv,
-  stdin: process.stdin,
-  stdout: process.stdout,
-  stderr: process.stderr
-})
-pnf.run(function() {
-  process.exit(0)
-})
+require('./cli.js')
